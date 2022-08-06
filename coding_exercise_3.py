c0684a26f9757d6a82975538b9ca896036c549b5
Sagar-Mohan-Jadhav <69190284+Sagar-Mohan-Jadhav@users.noreply.github.com>
2022-08-06 18:30:54 +0200

    update to code

--- coding_exercise_3.py
+++ coding_exercise_3.py
@@ -7,7 +7,7 @@ nearby_people = {'Rolf', 'Jen', 'Anna'}
 user_friends = set() # This is an empty set, like {}
 In this exercise, ask the user for the name of a friend. Add this name to the user_friends set provided.
 
-Finally, print out a set that contains only the name of the friend if the friend is in the nearby_people set.
+Finally, print out a set that contains only the name of the friend if friend is in the nearby_people set.
 
 You'll want to calculate the intersection between two sets, and print the result out.
 '''
